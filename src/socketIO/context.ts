00462e4aa21acc8febde7cc12f6d5bd16f865c16
pdh <huy@hmsp.com.au>
2022-02-17 14:43:07 +0700

    change link be in socket

--- src/socketIO/context.ts
+++ src/socketIO/context.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 import { io } from 'socket.io-client';
 
-export const socket = io('localhost:8080');
+export const socket = io('https://vercel.com/pdh372/hmsp-tool-management-be');
 export const SocketContext = React.createContext(socket);
